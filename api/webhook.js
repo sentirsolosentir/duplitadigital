c589d69fc6340b198e053f63410a1a366588c045
sentirsolosentir <sg.decarvalho@gmail.com>
2025-06-20 17:55:53 -0400

    webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   if (req.method === "POST") {
-    const intent = req.body.queryResult?.intent?.displayName || "";
+    const intent = req.body.queryResult?.intent?.displayName;
     let responseText = "";
 
     switch (intent) {
@@ -8,16 +8,14 @@ export default function handler(req, res) {
         responseText = "Hola, soy Duplita Digital. ¿Qué deseas saber?";
         break;
       case "Duplita":
-        responseText =
-          "Fui creada por Stephany, una publicista y psicóloga brillante. Estoy aquí para ayudarte 💫";
+        responseText = "Fui creada por Stephany, una publicista y psicóloga brillante. Estoy aquí para ayudarte 💫";
         break;
       default:
-        responseText =
-          "¡Duplita aún está aprendiendo eso! ¿Quieres preguntarme otra cosa?";
+        responseText = "¡Duplita aún está aprendiendo eso! ¿Quieres preguntarme otra cosa?";
     }
 
     res.status(200).json({ fulfillmentText: responseText });
   } else {
-    res.status(405).json({ message: "Solo acepto requests POST, corazoncito" });
+    res.status(405).json({ message: "Only POST requests allowed" });
   }
 }
